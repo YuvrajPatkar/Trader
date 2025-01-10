7d316c215f3001fecbe7a911590de6bf21ed72c2
YuvrajPatkar <patkar.y@etoncollege.org.uk>
2025-01-10 17:13:25 +0000

    Initial Commit

---
+++ Trader/TraderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraderApp.swift
+//  Trader
+//
+//  Created by Yuvraj Rahul Patkar on 10/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct TraderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TraderTests/TraderTests.swift
@@ -0,0 +1,17 @@
+//
+//  TraderTests.swift
+//  TraderTests
+//
+//  Created by Yuvraj Rahul Patkar on 10/01/2025.
+//
+
+import Testing
+@testable import Trader
+
+struct TraderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
